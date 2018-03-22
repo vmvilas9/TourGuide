f3b9dd213332850d6836f93dec85ec6a4e97411f
Vilas <vmvilas9@gmail.com>
2018-03-22 23:29:48 +0530

    variable changes

--- app/src/main/java/com/example/android/tourguide/RestrauntActivity.java
+++ app/src/main/java/com/example/android/tourguide/RestrauntActivity.java
@@ -33,33 +33,31 @@ import java.util.List;
 
 public class RestrauntActivity extends AppCompatActivity {
     public static final String LOG_TAG = RestrauntActivity.class.getName();
-    String i="0";
-    private GeoDataClient mgeoDataClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tour_list);
-
+        GeoDataClient mgeoDataClient;
         final ArrayList<Tour> tour = new ArrayList<>();
-        final TourAdapter adapter = new TourAdapter(this,tour);
-        final ListView listView = (ListView)findViewById(R.id.list);
-        mgeoDataClient = Places.getGeoDataClient(this,null);
-        mgeoDataClient.getPlaceById("ChIJCW8IJ-rsDzkRI1FpwEbBRwk","ChIJWat28iPtDzkR3r-0EOaCj0s").addOnCompleteListener
+        final TourAdapter adapter = new TourAdapter(this, tour);
+        final ListView listView = (ListView) findViewById(R.id.list);
+        mgeoDataClient = Places.getGeoDataClient(this, null);
+        mgeoDataClient.getPlaceById("ChIJCW8IJ-rsDzkRI1FpwEbBRwk", "ChIJWat28iPtDzkR3r-0EOaCj0s").addOnCompleteListener
                 (new OnCompleteListener<PlaceBufferResponse>() {
-            @Override
-            public void onComplete(@NonNull Task<PlaceBufferResponse> task) {
-                if(task.isSuccessful()){
-                    PlaceBufferResponse places = task.getResult();
-                    for (int i = 0;i<places.getCount();i++) {
-                        Place myPlace = places.get(i);
-                        tour.add(new Tour(myPlace.getName().toString(),myPlace.getAddress().toString(),myPlace.getPhoneNumber().toString(),myPlace.getRating()));
-                        Log.i(LOG_TAG,myPlace.getName().toString() + " " + places.getCount());
+                    @Override
+                    public void onComplete(@NonNull Task<PlaceBufferResponse> task) {
+                        if (task.isSuccessful()) {
+                            PlaceBufferResponse places = task.getResult();
+                            for (int i = 0; i < places.getCount(); i++) {
+                                Place myPlace = places.get(i);
+                                tour.add(new Tour(myPlace.getName().toString(), myPlace.getAddress().toString(), myPlace.getPhoneNumber().toString(), myPlace.getRating()));
+                                Log.i(LOG_TAG, myPlace.getName().toString() + " " + places.getCount());
+                            }
+                            places.release();
+                            listView.setAdapter(adapter);
+                        }
                     }
-                    places.release();
-                    listView.setAdapter(adapter);
-                }
-            }
-        });
+                });
     }
 
 }
